dbda938bed15ea1ee27685e0fff58a7441de6201
AslanKussein <kussein93@gmail.com>
2017-03-31 09:49:23 +0600

    Initial commit

---
+++ src/main/java/kz/bibabotan/lib/MyFramework.java
@@ -0,0 +1,11 @@
+package kz.bibabotan.lib;
+
+/**
+ * @author kusein-at;
+ * @since on 31.03.2017.
+ */
+public class MyFramework {
+    public static void makeAllProject(){
+        System.out.println("SUCCESS!");
+    }
+}
